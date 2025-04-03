0ba75ef6320481001d5e928d8f2f2575dcae1e87
EdOc-PS <eeuardooctavio@gmail.com>
2025-04-03 14:53:47 -0300

    feat: criação do react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export default function App() {
+
+
+  return (
+    <>
+      <main>
+        <section className='section_main'>
+          <div className='container_title'>
+          <i class="fi fi-brands-github"></i>
+          <h1>Perfil Github</h1>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
